546e879350244ada7d28b50438648091813425a8
Vieolent <miranda.l.bache@gmail.com>
2026-03-25 16:21:26 +0100

    lektion 711

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ enable :sessions
 @adminness = false
 
 get('/') do
-  rocks = SQLite3::Database.new("db/rocks.db")
+  rocks = SQLite3::Database.new("db/db.db")
   rocks.results_as_hash = true
   slim(:index)
 end
@@ -22,7 +22,7 @@ get('/my_rocks') do
   if session[:user_id] == nil
     redirect('/login')
   end
-  rocks = SQLite3::Database.new("db/rocks.db")
+  rocks = SQLite3::Database.new("db/db.db")
   rocks.results_as_hash = true
   @personal_rocks = rocks.execute("SELECT * FROM rocks WHERE owner_id = ? ORDER BY id DESC", [session[:user_id]]) #lägg till krav på inloggning
   slim(:my_rocks)
@@ -38,13 +38,13 @@ post('/new') do
   image = params[:image]
   rock_type = params[:rock_type]
   publicness = params[:publicness]
-  rocks = SQLite3::Database.new("db/rocks.db")  
+  rocks = SQLite3::Database.new("db/db.db")  
   rocks.execute("INSERT INTO rocks (name, description, rock_type, owner_id, img, publicness) VALUES (?, ?, ?, ?, ?, ?) ", [new_rock, description, rock_type, session[:user_id], image, publicness])
   redirect('/my_rocks')
 end
 
 post("/:id/delete") do
-  rocks = SQLite3::Database.new("db/rocks.db")
+  rocks = SQLite3::Database.new("db/db.db")
   removing = params[:id].to_i
   rocks.execute("DELETE FROM rocks WHERE id = ?", removing)
   redirect('/my_rocks')
@@ -52,14 +52,14 @@ end
 
 get("/:id/edit") do
   id = params[:id].to_i
-  db = SQLite3::Database.new("db/rocks.db")
+  db = SQLite3::Database.new("db/db.db")
   db.results_as_hash = true
   @selected_rock = db.execute("SELECT * FROM rocks WHERE id=?", id).first
   slim(:"/edit")
 end
 
 post("/:id/update") do
-  db = SQLite3::Database.new("db/rocks.db")  
+  db = SQLite3::Database.new("db/db.db")  
   id = params[:id].to_i
   name = params[:name]
   description = params[:description]
@@ -77,7 +77,7 @@ post("/new_user") do
   password = params[:password]
   confirm_password = params[:confirm_password]
 
-  users = SQLite3::Database.new("db/users.db")
+  users = SQLite3::Database.new("db/db.db")
   result=users.execute("SELECT id FROM users WHERE username=?", username)
 
   if result.empty?
@@ -108,7 +108,7 @@ post('/login') do
   password = params["password"]
 
 
-  users = SQLite3::Database.new("db/users.db")
+  users = SQLite3::Database.new("db/db.db")
   users.results_as_hash = true
   result=users.execute("SELECT id, password FROM users WHERE username=?", username)
 
@@ -133,12 +133,10 @@ post('/login') do
 end
 
 get('/browse') do
-  users = SQLite3::Database.new("db/users.db")
-  users.results_as_hash = true
-  rocks = SQLite3::Database.new("db/rocks.db")
-  rocks.results_as_hash = true
-  @public_rocks = rocks.execute("SELECT * FROM rocks WHERE publicness = 'true' ORDER BY id DESC")
-  if users.execute("SELECT adminstatus FROM users WHERE id = ?", session[:user_id]) == [{"adminstatus"=>"true"}]
+  db = SQLite3::Database.new("db/db.db")
+  db.results_as_hash = true
+  @public_rocks = db.execute("SELECT * FROM rocks WHERE publicness = 'true' ORDER BY id DESC")
+  if db.execute("SELECT adminstatus FROM users WHERE id = ?", session[:user_id]) == [{"adminstatus"=>"true"}]
     @adminness = true
   end 
   slim(:browse)
@@ -153,7 +151,7 @@ get('/my_boxes') do
   if session[:user_id] == nil
     redirect('/login')
   end
-  boxes = SQLite3::Database.new("db/boxes.db")
+  boxes = SQLite3::Database.new("db/db.db")
   boxes.results_as_hash = true
   @personal_boxes = boxes.execute("SELECT * FROM boxes WHERE owner_id = ? ORDER BY id DESC", [session[:user_id]])
   slim(:my_boxes)
@@ -161,9 +159,54 @@ end
 
 get('/:id/box') do
   id = params["id"]
-  boxes = SQLite3::Database.new("db/boxes.db")
-  boxes.results_as_hash = true
-  @boxinfo = boxes.execute("SELECT * FROM boxes WHERE id = ?", [id]).first
-  p @boxinfo
+  session[:box_id] = id
+  db = SQLite3::Database.new("db/db.db")
+  db.results_as_hash = true
+  @boxinfo = db.execute("SELECT * FROM boxes WHERE id = ?", [id]).first
+  @internalrocks = db.execute("SELECT * FROM rel_box_rocks INNER JOIN rocks ON rel_box_rocks.rock_id = rocks.id WHERE box_id = ? AND owner_id = ?", [id, session[:user_id]])
+  p @internalrocks
   slim(:box)
+end
+
+post("/:box_id/:rock_id/delete_from_box") do
+  db = SQLite3::Database.new("db/db.db")
+  removing = params[:box_id].to_i
+  removingage = params[:rock_id].to_i
+  db.execute("DELETE FROM rel_box_rocks WHERE rock_id = ? AND box_id = ?",[removingage, removing])
+  redirect('/my_rocks')
+end
+
+get("/:id/edit") do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/db.db")
+  db.results_as_hash = true
+  @selected_rock = db.execute("SELECT * FROM rocks WHERE id=?", id).first
+  slim(:"/edit")
+end
+
+get('/post_box') do
+  slim(:post_box)
+end
+
+post('/new_box') do
+  new_box = params[:new_box]
+  description = params[:description]
+  publicness = params[:publicness]
+  db = SQLite3::Database.new("db/db.db")  
+  db.execute("INSERT INTO boxes (name, description, owner_id, publicness) VALUES ( ?, ?, ?, ?) ", [new_box, description, session[:user_id], publicness])
+  redirect('/my_boxes')
+end
+
+get('/add_rock') do
+  db = SQLite3::Database.new("db/db.db")
+  db.results_as_hash = true
+  @public_rocks = db.execute("SELECT * FROM rocks WHERE publicness = 'true' OR owner_id= ? ORDER BY id DESC", [session[:user_id]])
+  slim(:add_rock)
+end
+
+post('/add_rocks') do
+  added_rocks = params[:rocks_selected]
+  p added_rocks
+
+  redirect('/my_boxes')
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,25 +1,38 @@
 require 'sqlite3'
 require 'bcrypt'
 
-rocks = SQLite3::Database.new("rocks.db")
+
+db = SQLite3::Database.new("db.db")
 
 
-def seed!(db)
+def seed4!(db)
   puts "Using db file: db/todos.db"
   puts "🧹 Dropping old tables..."
-  drop_tables(db)
+  drop_tables4(db)
   puts "🧱 Creating tables..."
-  create_tables(db)
+  create_tables4(db)
   puts "🍎 Populating tables..."
-  populate_tables(db)
+  populate_tables4(db)
   puts "✅ Done seeding the database!"
 end
 
-def drop_tables(db)
+def drop_tables4(db)
+  db.execute('DROP TABLE IF EXISTS users')
   db.execute('DROP TABLE IF EXISTS rocks')
+  db.execute('DROP TABLE IF EXISTS boxes')
+  db.execute('DROP TABLE IF EXISTS rel_box_rocks')
 end
 
-def create_tables(db)
+def create_tables4(db)
+  db.execute('CREATE TABLE rel_box_rocks (
+              rock_id INTEGER, 
+              box_id INTEGER,
+              PRIMARY KEY (rock_id, box_id),
+              FOREIGN KEY (rock_id) REFERENCES Rocks(id)
+                  ON DELETE CASCADE,
+              FOREIGN KEY (box_id) REFERENCES Boxes(id)
+                  ON DELETE CASCADE)')
+
   db.execute('CREATE TABLE rocks (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               name TEXT NOT NULL, 
@@ -28,79 +41,29 @@ def create_tables(db)
               owner_id INTEGER,
               img TEXT,
               publicness TEXT)')
-end
 
-def populate_tables(db)
-  db.execute('INSERT INTO rocks (name, description, rock_type, owner_id, img, publicness) VALUES ("test rock", "This is a test rock", "Grianite", 1, "definetly an image", "true")')
-end
-
-
-users = SQLite3::Database.new("users.db")
-
-
-def seed2!(db)
-  puts "Using db file: db/todos.db"
-  puts "🧹 Dropping old tables..."
-  drop_tables2(db)
-  puts "🧱 Creating tables..."
-  create_tables2(db)
-  puts "🍎 Populating tables..."
-  populate_tables2(db)
-  puts "✅ Done seeding the database!"
-end
-
-def drop_tables2(db)
-  db.execute('DROP TABLE IF EXISTS users')
-  db.execute('DROP TABLE IF EXISTS rocks')
-end
-
-def create_tables2(db)
   db.execute('CREATE TABLE users (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               username TEXT NOT NULL, 
               password TEXT NOT NULL,
               adminstatus TEXT,
               geologstatus TEXT)')
-end
 
-def populate_tables2(db)
-  password = BCrypt::Password.create("12345")
-  db.execute('INSERT INTO users (username, password, adminstatus, geologstatus) VALUES ("Admin", ?, "true", "true")', [password])
-end
-
-
-
-boxes = SQLite3::Database.new("boxes.db")
-
-
-def seed3!(db)
-  puts "Using db file: db/todos.db"
-  puts "🧹 Dropping old tables..."
-  drop_tables3(db)
-  puts "🧱 Creating tables..."
-  create_tables3(db)
-  puts "🍎 Populating tables..."
-  populate_tables3(db)
-  puts "✅ Done seeding the database!"
-end
-
-def drop_tables3(db)
-  db.execute('DROP TABLE IF EXISTS users')
-  db.execute('DROP TABLE IF EXISTS rocks')
-  db.execute('DROP TABLE IF EXISTS boxes')
-end
-
-def create_tables3(db)
   db.execute('CREATE TABLE boxes (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               name TEXT NOT NULL, 
               description TEXT,
               publicness TEXT,
               owner_id INTEGER)')
+
 end
 
-def populate_tables3(db)
+def populate_tables4(db)
+  db.execute('INSERT INTO rocks (name, description, rock_type, owner_id, img, publicness) VALUES ("test rock", "This is a test rock", "Grianite", 1, "definetly an image", "true")')
+  db.execute('INSERT INTO rel_box_rocks (rock_id, box_id) VALUES (1, 1)')
+  password = BCrypt::Password.create("12345")
+  db.execute('INSERT INTO users (username, password, adminstatus, geologstatus) VALUES ("Admin", ?, "true", "true")', [password])
   db.execute('INSERT INTO boxes (name, description, publicness, owner_id) VALUES ("Testbox", "This is the default box", "true", 1)')
 end
 
-seed3!(boxes)
+seed4!(db)
